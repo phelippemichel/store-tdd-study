af63b355810a10e7f513974fcc98619608c57f24
phelippemichel <phelippe.michel@gmail.com>
2023-11-21 17:17:28 -0300

    feat: add entity

---
+++ Store.Domain/Entities/Entity.cs
@@ -0,0 +1,14 @@
+using System.Data.Common;
+
+namespace Store.Domain.Entities
+{
+    public abstract class Entity
+    {
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; private set; }
+    }
+}
