f06ca7f8f7f31b12cb6c5338b8ae9ccab6595d97
phelippemichel <phelippe.michel@gmail.com>
2023-11-30 15:13:19 -0300

    test: add class tests

--- Store.Tests/Domain/OrderTests.cs
+++ Store.Tests/Domain/OrderTests.cs
@@ -8,8 +8,6 @@ namespace Store.Tests.Domain
     {
         private readonly Customer _customer;
         private readonly Product _product;
-        private readonly Quantity _quantity;
-        private readonly DeliveryFee _deliveryFee;
         private readonly Discount _discount;
     }
 
@@ -17,8 +15,6 @@ namespace Store.Tests.Domain
     {
         _customer = new Customer("Phelippe Michel");
         _product = new Product("Produto 1", 10, true);
-        _quantity = new Quantity(1);
-        _deliveryFee = new DeliveryFee(10);
         _discount = new Discount(10, DateTime.Now.AddDays(5));
     }
 
@@ -26,7 +22,7 @@ namespace Store.Tests.Domain
     [TestCategory("Domain")]
     public void Should_Return_Sucess_Had_Active_Order()
     {
-        var order = new Order(_customer, _deliveryFee, _discount);
+        var order = new Order(_customer, _product, _discount);
         Assert.AreEqual(8, order.Number.Length);
     }
 }
