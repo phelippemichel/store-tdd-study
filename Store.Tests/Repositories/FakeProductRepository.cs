7722a6a349b9e960460d0b769704f79d3ef601ee
phelippemichel <phelippe.michel@gmail.com>
2024-01-03 16:36:13 -0300

    refactor: correct dependecy names

--- Store.Tests/Repositories/FakeProductRepository.cs
+++ Store.Tests/Repositories/FakeProductRepository.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using Store.Domain.Entities;
 using Store.Domain.Repositories.Interfaces;
 
@@ -8,11 +10,13 @@ namespace Store.Tests.Repositories
         public IEnumerable<Product> Get(IEnumerable<Guid> ids)
         {
             IList<Product> products = new List<Product>();
-            products.Add(new Product("Produto 01", 10, true));
-            products.Add(new Product("Produto 02", 10, true));
-            products.Add(new Product("Produto 03", 10, true));
-            products.Add(new Product("Produto 04", 10, false));
-            products.Add(new Product("Produto 05", 10, false));
+            products.Add(new Product("Product 01", 10, true));
+            products.Add(new Product("Product 02", 10, true));
+            products.Add(new Product("Product 03", 10, true));
+            products.Add(new Product("Product 04", 10, false));
+            products.Add(new Product("Product 05", 10, false));
+
+            return products;
         }
     }
 }
